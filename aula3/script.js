ddd685922b44b5ced96dcd26bea2b4373a1c19ea
Anna Cristina <annacristinaferreiraalves@gmail.com>
2025-09-08 23:21:55 -0300

    aula 3

---
+++ aula3/script.js
@@ -0,0 +1,7 @@
+"use strict";
+let produto = 'livro';
+const preco = 200;
+const carro = {
+    marca: 'Audi',
+    portas: 4
+};
---
+++ aula3/script.ts
@@ -0,0 +1,12 @@
+let produto: string = 'livro';
+const preco: number = 200;
+
+const carro: {
+    marca: string;
+    portas: number
+} = {
+    marca: 'Audi',
+    portas: 4
+}
+
+
