602a3084632ce21bb31f7a04873c845a610d32db
Anna Cristina <annacristinaferreiraalves@gmail.com>
2025-09-19 22:43:00 -0300

    aula 8

--- aula8/script.js
+++ aula8/script.js
@@ -1,39 +1,51 @@
 "use strict";
 class Produto {
     nome;
-    preco;
-    constructor(nome, preco) {
+    constructor(nome) {
         this.nome = nome;
-        this.preco = preco;
-    }
-    precoReal() {
-        return `R$ ${this.preco}`;
     }
 }
-const livro = new Produto('O Morro dos Ventos Uivanes', 120);
+const livro = new Produto('O Morro dos Ventos Uivanes');
 console.log(livro instanceof Produto);
-class Livro {
+class Livro extends Produto {
     autor;
-    constructor(autor) {
+    constructor(nome, autor) {
+        super(nome);
         this.autor = autor;
+        this.nome = nome;
     }
 }
-class Jogo {
+class Jogo extends Produto {
     jogadores;
-    constructor(jogadores) {
+    constructor(jogadores, nome) {
+        super(nome);
         this.jogadores = jogadores;
     }
 }
 function buscarProduto(busca) {
     if (busca === 'O Morro dos Ventos Uivantes') {
-        return new Livro('Emily Bronte');
+        return new Livro('O Morro dos Ventos Uivantes', 'Emily Bronte');
     }
     if (busca === 'Dark Souls') {
-        return new Jogo(1);
+        return new Jogo(1, 'Dark Souls');
     }
     return null;
 }
 const produto = buscarProduto('O Morro dos Ventos Uivantes');
+if (produto instanceof Jogo) {
+    console.log(produto.jogadores);
+}
 if (produto instanceof Livro) {
     console.log(produto.autor);
+}
+if (produto instanceof Produto) {
+    console.log("sim");
+    console.log(produto.nome);
+}
+else {
+    console.log("nao");
+}
+const link = document.getElementById('origamid');
+if (link instanceof HTMLAnchorElement) {
+    link.href.replace('http', 'https');
 }
--- aula8/script.ts
+++ aula8/script.ts
@@ -1,44 +1,59 @@
 class Produto {
     nome: string;
-    preco: number;
-    constructor(nome: string, preco: number) {
+    constructor(nome: string) {
         this.nome = nome;
-        this.preco = preco;
-    }
-    precoReal() {
-        return `R$ ${this.preco}`
     }
 }
 
-const livro = new Produto('O Morro dos Ventos Uivanes', 120)
+const livro = new Produto('O Morro dos Ventos Uivanes')
 console.log(livro instanceof Produto);
 
-class Livro {
+class Livro extends Produto {
     autor: string;
-    constructor(autor: string) {
+    constructor(nome: string, autor: string) {
+        super(nome);
         this.autor = autor;
+        this.nome = nome;
     }
 }
 
-class Jogo {
+class Jogo extends Produto {
     jogadores: number;
-    constructor(jogadores: number) {
+    constructor(jogadores: number, nome: string) {
+        super(nome);
         this.jogadores = jogadores;
     }
 }
 
 function buscarProduto(busca: string) {
     if(busca === 'O Morro dos Ventos Uivantes') {
-        return new Livro('Emily Bronte')
+        return new Livro('O Morro dos Ventos Uivantes', 'Emily Bronte');
     }
     if(busca === 'Dark Souls') {
-        return new Jogo(1);
+        return new Jogo(1, 'Dark Souls');
     }
     return null;
 }
 
 const produto = buscarProduto('O Morro dos Ventos Uivantes')
 
+if(produto instanceof Jogo) {
+    console.log(produto.jogadores);
+}
+
 if(produto instanceof Livro) {
     console.log(produto.autor);
+}
+
+if(produto instanceof Produto) {
+    console.log("sim");
+    console.log(produto.nome)
+}else {
+    console.log("nao")
+}
+
+const link = document.getElementById('origamid');
+
+if(link instanceof HTMLAnchorElement) {
+    link.href.replace('http', 'https');
 }
